0fbee698e274ac9c903c2e6bf7dd75ad91fc4760
Chandan Shaw <dev.chandan.shaw@gmail.com>
2025-06-14 17:35:07 +0530

    Updated pom file

--- src/main/java/in/chandan/main/ProductReviewApplication.java
+++ src/main/java/in/chandan/main/ProductReviewApplication.java
@@ -1,18 +1,11 @@
 package in.chandan.main;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProductReviewApplication {
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
-
-		System.setProperty("DB_URL", dotenv.get("DB_URL"));
-		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
-		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
-		System.setProperty("PORT", dotenv.get("PORT"));
 		SpringApplication.run(ProductReviewApplication.class, args);
 	}
 }
